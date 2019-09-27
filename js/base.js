b50d2529539702065dd8de930edc7ab7a54480c0
Jérémie Bayart <jeremie.bayart@hotmail.fr>
2019-09-27 23:08:28 +0100

    Deployed 988839a with MkDocs version: 1.0.4

--- js/base.js
+++ js/base.js
@@ -18,6 +18,39 @@ var searchIndex = null;
 var showPageToc = true;
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
+
+var client = new HttpClient();
+
+client.get('https://api.github.com/repos/cyoung1024/Loop/commits/dev-spike', function(response) {
+	var obj = JSON.parse(response); 
+	var dateString = obj["commit"]["author"]["date"];
+	var date = new Date(Date.parse(dateString));
+	var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit',
+    minute:'2-digit'};
+    $('#date-spike').html(date.toLocaleDateString('fr-FR', options));
+});
+
+client.get('https://api.github.com/repos/Loopkit/Loop/commits/dev', function(response) {
+	var obj = JSON.parse(response); 
+	var dateString = obj["commit"]["author"]["date"];
+	var date = new Date(Date.parse(dateString));
+	var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit',
+    minute:'2-digit'};
+    $('#date-dev').html(date.toLocaleDateString('fr-FR', options));
+});
+
 var Keys = {
   ENTER:  13,
   ESCAPE: 27,
